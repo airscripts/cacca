a4efadcdc52ddefb549de01bb6c469a502d4fb32
Airscript <francesco@airscript.it>
2023-09-15 21:16:45 +0200

    feat(constants): add empty literal constant

--- src/cacca/stubs.py
+++ src/cacca/stubs.py
@@ -1,7 +1,8 @@
 from typing import Literal
 
+from cacca.constants import EMPTY_LITERAL
 from cacca.types import Args, KwArgs
 
 
 def empty(*_: Args, **__: KwArgs) -> Literal["EMPTY"]:
-    return "EMPTY"
+    return EMPTY_LITERAL
